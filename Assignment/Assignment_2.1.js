484234cafe1de73ba611749d884c96ab6cc9c9cb
abshriva <shaky2108@gmail.com>
2017-04-24 18:18:03 +0530

    Add
    Add

---
+++ Assignment/Assignment_2.1.js
@@ -0,0 +1,4 @@
+var userInformation = function (userName, salary) {
+    return "Hello My Name is " + userName + " & my salary is " + salary;
+};
+console.log(userInformation("Abhishek", 50000));
---
+++ Assignment/Assignment_2.1.ts
@@ -0,0 +1,6 @@
+var userInformation = (userName: string , salary: number) => 
+{
+  return `Hello My Name is ${userName} & my salary is ${salary}`;
+};
+
+console.log(userInformation("Abhishek",50000));
